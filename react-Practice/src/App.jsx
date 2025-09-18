3ae455635e0901acd3ce6ef61c9c4175020741fa
kaifyyy <mkaifm728@gmail.com>
2025-09-19 00:17:14 +0530

    initial commits

--- react-Practice/src/App.jsx
+++ react-Practice/src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <h2>Hello Guys</h2>
+      <h2 className='text-3xl bg-'>Hello Guys</h2>
       <h1>Inshallah!</h1>
       <h2>James Bond</h2>
     </div>
