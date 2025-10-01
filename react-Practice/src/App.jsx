a7408a8b69c713fdf63934a4fa65f003cbd439bc
kaifyyy <mkaifm728@gmail.com>
2025-10-02 01:56:13 +0530

    commits

--- react-Practice/src/App.jsx
+++ react-Practice/src/App.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+
+  const [username, setusername] = useState('gym- body builder')
+
   return (
     <div>
-      <h2 className='text-3xl bg-'>Hello Guys</h2>
-      <h1>Inshallah!</h1>
-      <h2>James Bond</h2>
-    </div>
+
+      <form>
+        <input value="kaify"
+        type="text px-4 py-3" placeholder="Enter your name" />
+        <button type="submit" className='px-4 py-3 font-semibold'>Sub mit Here</button>
+      </form>
+    </div> 
   )
 }
 
