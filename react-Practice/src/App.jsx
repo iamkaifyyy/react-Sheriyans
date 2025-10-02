d24c5dd239fe8c248ab0ed948b04c73e07dafd2f
kaifyyy <mkaifm728@gmail.com>
2025-10-02 23:59:19 +0530

    commit

--- react-Practice/src/App.jsx
+++ react-Practice/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from './src/components/Header'
 import Navbar from './src/components/Navbar'
+import Footer from './src/components/Footer'
 
 
 const App = () => {
