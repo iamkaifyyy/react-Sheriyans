e5bf89064dabf3097f4f20b355c3769a6011f2f3
kaifyyy <mkaifm728@gmail.com>
2025-10-02 23:51:59 +0530

    commit

--- react-Practice/src/App.jsx
+++ react-Practice/src/App.jsx
@@ -3,17 +3,16 @@ import React from 'react'
 const App = () => {
   return (
     <>
-      <nav className='flex justify-between items-center p-4'  >
-        <h2>kaifyyy</h2>
-        <div className='flex gap-8 items-center '>
-          <h4>About</h4>
-          <h4>Contact</h4>
-          <h4>Services</h4>
-          <h4>Your Account</h4>
+      <nav className='bg-emerald-950 flex py-5 px-10 items-center justify-center' >
+        <h2 className='text-2xl'>kaifyyy</h2>
+        <div className='flex gap-10 items-center '>
+          <h4 className='text-xl'>About</h4>
+          <h4 className='text-xl'>Contact</h4>
+          <h4 className='text-xl'>Services</h4>
+          <h4 className='text-xl'>Your Account</h4>
         </div>
       </nav>
     </>
   )
 }
 
- 
