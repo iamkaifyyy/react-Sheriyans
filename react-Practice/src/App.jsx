7fd1a13c6a1eef6d19af4b6b947f2a5547664da3
kaifyyy <mkaifm728@gmail.com>
2025-10-02 18:42:12 +0530

    commits

--- react-Practice/src/App.jsx
+++ react-Practice/src/App.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>Header</div>
+    <>
+    <Headers />
+    <div>App</div>
+    </>
   )
 }
 
---
+++ react-Practice/src/components/Headers.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Headers = () => {
+  return (
+    <div>Mai Hoon Headers</div>
+  )
+}
+
+export default Headers
